89b0afed0af28ea730ee22ba4da68a55df060263
Fiskee <fisker@gmail.com>
2019-08-17 14:35:35 +0200

    Best cone angle casts

--- Classes/Spell/Cast.lua
+++ Classes/Spell/Cast.lua
@@ -46,3 +46,133 @@ function Spell:Cast(Unit)
     end
     return false
 end
+
+function Spell:CastBestConeEnemy(Length, Angle, MinHit, TTD)
+	if not self:IsReady() then
+		return false
+    end
+    MinHit = MinHit or 1
+    TTD = TTD or 0
+    local Table, TableCount = DMW.Player:GetEnemies("player", Length)
+    if TableCount < MinHit then
+        return false
+    end
+	local PX, PY, PZ = DMW.Player.PosX, DMW.Player.PosY, DMW.Player.PosZ
+    local ConeTable = {}
+    local X, Y, Z, AngleToUnit
+    for _, Unit in pairs(Table) do
+        if Unit.TTD >= TTD then
+            X, Y, Z = Unit.PosX, Unit.PosY, Unit.PosZ
+            AngleToUnit = rad(atan2(Y - PY, X - PX))
+            if AngleToUnit < 0 then
+                AngleToUnit = rad(360 + atan2(Y - PY, X - PX))
+            end
+            tinsert(ConeTable, AngleToUnit)
+        end
+	end
+    local Facing, BestAngle, MostHit, Units = 0, 0, 0, 0
+    local AngleToUnit, AngleDifference, ShortestAngle, FinalAngle
+	while Facing <= 6.2 do
+		Units = 0
+		for i = 1, #ConeTable do
+			AngleToUnit = ConeTable[i]
+			AngleDifference = Facing > AngleToUnit and Facing - AngleToUnit or AngleToUnit - Facing
+			ShortestAngle = AngleDifference < math.pi and AngleDifference or math.pi * 2 - AngleDifference
+			FinalAngle = ShortestAngle * 180 / math.pi
+			if FinalAngle < Angle / 2 then
+				Units = Units + 1
+			end
+		end
+		if Units > MostHit then
+			MostHit = Units
+			BestAngle = Facing
+		end
+		Facing = Facing + 0.05
+	end
+    if MostHit >= MinHit then
+        local CurrentFacing = ObjectFacing("player")
+		local mouselookActive = false
+		if IsMouselooking() then
+			mouselookActive = true
+			MouselookStop()
+			TurnOrActionStop()
+			MoveAndSteerStop()
+		end
+		FaceDirection(BestAngle, true)
+		CastSpellByName(self.SpellName)
+		FaceDirection(CurrentFacing)
+		if mouselookActive then
+			MouselookStart()
+		end
+		C_Timer.After(0.1, function()
+			FaceDirection(ObjectFacing("player"), true)
+        end)
+		return true
+	end
+	return false
+end
+
+function Spell:CastBestConeFriend(Length, Angle, MinHit, HP)
+	if not self:IsReady() then
+		return false
+    end
+    MinHit = MinHit or 1
+    HP = HP or 100
+    local Table, TableCount = DMW.Player:GetEnemies("player", Length)
+    if TableCount < MinHit then
+        return false
+    end
+	local PX, PY, PZ = DMW.Player.PosX, DMW.Player.PosY, DMW.Player.PosZ
+    local ConeTable = {}
+    local X, Y, Z, AngleToUnit
+    for _, Unit in pairs(Table) do
+        if Unit.HP <= HP then
+            X, Y, Z = Unit.PosX, Unit.PosY, Unit.PosZ
+            AngleToUnit = rad(atan2(Y - PY, X - PX))
+            if AngleToUnit < 0 then
+                AngleToUnit = rad(360 + atan2(Y - PY, X - PX))
+            end
+            tinsert(ConeTable, AngleToUnit)
+        end
+	end
+    local Facing, BestAngle, MostHit, Units = 0, 0, 0, 0
+    local AngleToUnit, AngleDifference, ShortestAngle, FinalAngle
+	while Facing <= 6.2 do
+		Units = 0
+		for i = 1, #ConeTable do
+			AngleToUnit = ConeTable[i]
+			AngleDifference = Facing > AngleToUnit and Facing - AngleToUnit or AngleToUnit - Facing
+			ShortestAngle = AngleDifference < math.pi and AngleDifference or math.pi * 2 - AngleDifference
+			FinalAngle = ShortestAngle * 180 / math.pi
+			if FinalAngle < Angle / 2 then
+				Units = Units + 1
+			end
+		end
+		if Units > MostHit then
+			MostHit = Units
+			BestAngle = Facing
+		end
+		Facing = Facing + 0.05
+	end
+    if MostHit >= MinHit then
+        local CurrentFacing = ObjectFacing("player")
+		local mouselookActive = false
+		if IsMouselooking() then
+			mouselookActive = true
+			MouselookStop()
+			TurnOrActionStop()
+			MoveAndSteerStop()
+		end
+		FaceDirection(BestAngle, true)
+		CastSpellByName(self.SpellName)
+		FaceDirection(CurrentFacing)
+		if mouselookActive then
+			MouselookStart()
+		end
+		C_Timer.After(0.1, function()
+			FaceDirection(ObjectFacing("player"), true)
+        end)
+		return true
+	end
+	return false
+end
