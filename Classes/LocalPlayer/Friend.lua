a657cbef6da18f0548fc04ec04a7fb7acc7156c5
Fiskee <fisker@gmail.com>
2019-08-17 12:15:13 +0200

    HP here

--- Classes/LocalPlayer/Friend.lua
+++ Classes/LocalPlayer/Friend.lua
@@ -13,17 +13,17 @@ function LocalPlayer:GetFriends(Yards)
     return Table, Count
 end
 
-function LocalPlayer:GetFriendsRect(Length, Width, TTD)
+function LocalPlayer:GetFriendsRect(Length, Width, HP)
     local Count = 0
     local Table, TableCount = self:GetFriends(Length)
     if TableCount > 0 then
-        TTD = TTD or 0
+        HP = HP or 100
         local Facing = ObjectFacing(self.Pointer)
         local nlX, nlY, nlZ = GetPositionFromPosition(self.PosX, self.PosY, self.PosZ, Width/2, Facing + math.rad(90), 0)
         local nrX, nrY, nrZ = GetPositionFromPosition(self.PosX, self.PosY, self.PosZ, Width/2, Facing + math.rad(270), 0)
         local frX, frY, frZ = GetPositionFromPosition(nrX, nrY, nrZ, Length, Facing, 0)
         for _, Unit in pairs(Table) do
-            if Unit.PosX > nlX and Unit.PosX < frX and Unit.PosY > nlY and Unit.PosY < frY and Unit.TTD >= TTD then
+            if Unit.PosX > nlX and Unit.PosX < frX and Unit.PosY > nlY and Unit.PosY < frY and Unit.HP <= HP then
                 Count = Count + 1
             end
         end
@@ -31,14 +31,14 @@ function LocalPlayer:GetFriendsRect(Length, Width, TTD)
     return Count
 end
 
-function LocalPlayer:GetFriendsCone(Length, Angle, TTD)
+function LocalPlayer:GetFriendsCone(Length, Angle, HP)
     local Count = 0
     local Table, TableCount = self:GetFriends(Length)
     if TableCount > 0 then
-        TTD = TTD or 0
+        HP = HP or 100
         local Facing = ObjectFacing(self.Pointer)
         for _, Unit in pairs(Table) do
-            if Unit.TTD >= TTD and UnitIsFacing(self.Pointer, Unit.Pointer, Angle/2) then
+            if Unit.HP <= HP and UnitIsFacing(self.Pointer, Unit.Pointer, Angle/2) then
                 Count = Count + 1
             end
         end
