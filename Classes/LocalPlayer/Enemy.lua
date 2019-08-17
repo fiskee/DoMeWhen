e10dc9b0e28474afd332539becdc6c10ef504aa2
Fiskee <fisker@gmail.com>
2019-08-17 06:55:17 +0200

    Split player functions

---
+++ Classes/LocalPlayer/Enemy.lua
@@ -0,0 +1,58 @@
+local DMW = DMW
+local LocalPlayer = DMW.Classes.LocalPlayer
+
+function LocalPlayer:AutoTarget(Yards)
+    if not self.Target and self.Combat then
+        for k,v in pairs(DMW.Enemies) do
+            if v.Distance <= Yards then
+                TargetUnit(v.Pointer)
+                return true
+            end
+        end
+    end
+end
+
+function LocalPlayer:GetEnemies(Yards)
+    local Table = {}
+    local Count = 0
+    for _, v in pairs(DMW.Enemies) do
+        if v:GetDistance(self) <= Yards then
+            table.insert(Table, v)
+            Count = Count + 1
+        end
+    end
+    return Table, Count
+end
+
+function LocalPlayer:GetEnemiesRect(Length, Width, TTD)
+    local Count = 0
+    local Table, TableCount = self:GetEnemies(Length)
+    if TableCount > 0 then
+        TTD = TTD or 0
+        local Facing = ObjectFacing(self.Pointer)
+        local nlX, nlY, nlZ = GetPositionFromPosition(self.PosX, self.PosY, self.PosZ, Width/2, Facing + math.rad(90), 0)
+        local nrX, nrY, nrZ = GetPositionFromPosition(self.PosX, self.PosY, self.PosZ, Width/2, Facing + math.rad(270), 0)
+        local frX, frY, frZ = GetPositionFromPosition(nrX, nrY, nrZ, Length, Facing, 0)
+        for _, Unit in pairs(Table) do
+            if Unit.PosX > nlX and Unit.PosX < frX and Unit.PosY > nlY and Unit.PosY < frY and Unit.TTD >= TTD then
+                Count = Count + 1
+            end
+        end
+    end
+    return Count
+end
+
+function LocalPlayer:GetEnemiesCone(Length, Angle, TTD)
+    local Count = 0
+    local Table, TableCount = self:GetEnemies(Length)
+    if TableCount > 0 then
+        TTD = TTD or 0
+        local Facing = ObjectFacing(self.Pointer)
+        for _, Unit in pairs(Table) do
+            if Unit.TTD >= TTD and UnitIsFacing(self.Pointer, Unit.Pointer, Angle/2) then
+                Count = Count + 1
+            end
+        end
+    end
+    return Count
+end
