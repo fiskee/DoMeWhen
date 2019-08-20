fe438dc38e4963d10f263bb9baab09ee4a9fbebf
Fiskee <fisker@gmail.com>
2019-08-21 00:27:50 +0200

    Rogue Spells

--- Enums/Spells/Rogue.lua
+++ Enums/Spells/Rogue.lua
@@ -2,11 +2,67 @@ local Spells = DMW.Enums.Spells
 
 Spells.ROGUE = {
     Assassination = {
-        Abilities = {},
-        Buffs = {},
-        Debuffs = {},
-        Talents = {},
-        Traits = {}
+        Abilities = {
+            CrimsonTempest = {SpellID = 121411},
+            CripplingPoison = {SpellID = 3408},
+            DeadlyPoison = {SpellID = 2823},
+            Envenom = {SpellID = 32645},
+            Evasion = {SpellID = 5277},
+            Eviscerate = {SpellID = 196819},
+            Exsanguinate = {SpellID = 200806},
+            FanOfKnives = {SpellID = 51723},
+            Garrote = {SpellID = 703},
+            KidneyShot = {SpellID = 408},
+            Mutilate = {SpellID = 1329},
+            PoisonedKnife = {SpellID = 185565},
+            Rupture = {SpellID = 1943},
+            ToxicBlade = {SpellID = 245388},
+            Vendetta = {SpellID = 79140},
+            WoundPoison = {SpellID = 8679}
+        },
+        Buffs = {
+            CripplingPoison = 3408,
+            DeadlyPoison = 2823,
+            ElaboratePlanning = 193641,
+            Envenom = 32645,
+            HiddenBlades = 270070,
+            MasterAssassin = 256735,
+            Subterfuge = 115192,
+            WoundPoison = 8679
+        },
+        Debuffs = {
+            CrimsonTempest = {SpellID = 121411},
+            CripplingPoison = {SpellID = 3409},
+            DeadlyPoison = {SpellID = 2818},
+            Garrote = {SpellID = 703, BaseDuration = 18},
+            KidneyShot = {SpellID = 408},
+            Rupture = {SpellID = 1943},
+            ToxicBlade = {SpellID = 245389},
+            Vendetta = {SpellID = 79140},
+            WoundPoison = {SpellID = 8680}
+        },
+        Talents = {
+            Blindside = 22339,
+            CrimsonTempest = 23174,
+            ElaboratePlanning = 22338,
+            Elusiveness = 22123,
+            Exsanguinate = 22344,
+            HiddenBlades = 22133,
+            InternalBleeding = 19245,
+            IronWire = 23037,
+            MasterAssassin = 23022,
+            MasterPoisoner = 22337,
+            Nightstalker = 22331,
+            PoisonBomb = 21186,
+            Subterfuge = 22332,
+            ToxicBlade = 23015,
+            VenomRush = 22343
+        },
+        Traits = {
+            DoubleDose = 273007,
+            EchoingBlades = 287649,
+            ShroudedSuffocation = 278666
+        }
     },
     Outlaw = {
         Abilities = {},
@@ -23,8 +79,23 @@ Spells.ROGUE = {
         Traits = {}
     },
     All = {
-        Abilities = {},
-        Buffs = {},
+        Abilities = {
+            Blind = {SpellID = 2094},
+            CheapShot = {SpellID = 1833},
+            CloakOfShadows = {SpellID = 31224},
+            CrimsonVial = {SpellID = 185311},
+            Feint = {SpellID = 1966},
+            Kick = {SpellID = 1766},
+            MarkedForDeath = {SpellID = 137619},
+            Sap = {SpellID = 6770},
+            Stealth = {SpellID = 115191},
+            TricksOfTheTrade = {SpellID = 57934},
+            Vanish = {SpellID = 1856}
+        },
+        Buffs = {
+            Stealth = 115191,
+            Vanish = 11327
+        },
         Debuffs = {}
     }
 }
---
+++ Rotations/Rogue/Assassination.lua
@@ -0,0 +1,65 @@
+local DMW = DMW
+local Rogue = DMW.Rotations.ROGUE
+local Player, Buff, Debuff, Spell, Target, Trait, Talent, Item, GCD, HUD, Player5Y, Player5YC, Player10Y, Player10YC
+local UI = DMW.UI
+local Rotation = DMW.Helpers.Rotation
+local Setting = DMW.Helpers.Rotation.Setting
+
+local function CreateSettings()
+    if not UI.HUD.Options then
+        UI.HUD.Options = {
+            CDs = {
+                [1] = {Text = "Cooldowns |cFF00FF00Auto", Tooltip = ""},
+                [2] = {Text = "Cooldowns |cFFFFFF00Always On", Tooltip = ""},
+                [3] = {Text = "Cooldowns |cffff0000Disabled", Tooltip = ""}
+            },
+            Mode = {
+                [1] = {Text = "Rotation Mode |cFF00FF00Auto", Tooltip = ""},
+                [2] = {Text = "Rotation Mode |cFFFFFF00Single", Tooltip = ""}
+            },
+            Interrupts = {
+                [1] = {Text = "Interrupts |cFF00FF00Enabled", Tooltip = ""},
+                [2] = {Text = "Interrupts |cffff0000Disabled", Tooltip = ""}
+            }
+        }
+
+        UI.AddHeader("DPS")
+        UI.AddToggle("Vendetta", "Use Vendetta", true)
+        UI.AddToggle("Vanish", "Use Vanish", true)
+        UI.AddHeader("Defensive")
+        UI.AddToggle("Healthstone", "Use Healthstone", true)
+        UI.AddRange("Healthstone HP", "HP to use Healthstone", 0, 100, 1, 60)
+    end
+end
+
+local function Locals()
+    Player = DMW.Player
+    Buff = Player.Buffs
+    Debuff = Player.Debuffs
+    Spell = Player.Spells
+    Talent = Player.Talents
+    Trait = Player.Traits
+    Item = Player.Items
+    Target = Player.Target or false
+    GCD = Player:GCD()
+    HUD = DMW.Settings.profile.HUD
+end
+
+local function OOC()
+    if not Player.Moving then
+        
+    end
+end
+
+function Rogue.Assassination()
+    Locals()
+    CreateSettings()
+    if not Player.Combat then
+        OOC()
+    end
+    if Rotation.Active() then
+        if (Target and Target.ValidEnemy) or Player.Combat then
+            Player:AutoTarget(5)
+        end
+    end
+end
