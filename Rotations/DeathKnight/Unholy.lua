974965a5e2235fe14e6f89aa61a7fcb446c57ec6
Fiskee <fisker@gmail.com>
2020-07-19 09:41:40 +0200

    Empty files

---
+++ Rotations/DeathKnight/Unholy.lua
@@ -0,0 +1,23 @@
+local DMW = DMW
+local DeathKnight = DMW.Rotations.DEATHKNIGHT
+local Player, Buff, Debuff, Spell, Target, Pet, Trait, GCD, Pet5Y, Pet5YC, HUD, Player40Y, Player40YC, Player5Y, Player5YC
+local UI = DMW.UI
+local Rotation = DMW.Helpers.Rotation
+
+local function CreateSettings()
+    if not UI.HUD.Options then
+
+    end
+end
+
+local function Locals()
+    Player = DMW.Player
+    Buff = Player.Buffs
+    Debuff = Player.Debuffs
+    Spell = Player.Spells
+    Trait = Player.Traits
+    Target = Player.Target or false
+    Pet = Player.Pet or false
+    GCD = Player:GCD()
+    HUD = DMW.Settings.profile.HUD
+end
---
+++ Rotations/DemonHunter/Havoc.lua
@@ -0,0 +1,22 @@
+local DMW = DMW
+local DemonHunter = DMW.Rotations.DEMONHUNTER
+local Player, Buff, Debuff, Spell, Target, Trait, GCD, HUD, Player40Y, Player40YC, Player5Y, Player5YC
+local UI = DMW.UI
+local Rotation = DMW.Helpers.Rotation
+
+local function CreateSettings()
+    if not UI.HUD.Options then
+
+    end
+end
+
+local function Locals()
+    Player = DMW.Player
+    Buff = Player.Buffs
+    Debuff = Player.Debuffs
+    Spell = Player.Spells
+    Trait = Player.Traits
+    Target = Player.Target or false
+    GCD = Player:GCD()
+    HUD = DMW.Settings.profile.HUD
+end
