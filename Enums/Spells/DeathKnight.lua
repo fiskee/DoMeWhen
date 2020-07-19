7c649f65008664a34bc0337320b6822737137ce2
Fiskee <fisker@gmail.com>
2020-07-19 10:35:18 +0200

    Blood spells

--- Enums/Spells/DeathKnight.lua
+++ Enums/Spells/DeathKnight.lua
@@ -2,11 +2,78 @@ local Spells = DMW.Enums.Spells
 
 Spells.DEATHKNIGHT = {
     Blood = {
-        Abilities = {},
-        Buffs = {},
-        Debuffs = {},
-        Talents = {},
-        Traits = {}
+        Abilities = {
+            AntiMagicBarrier = {SpellID = 205727},
+            AntiMagicShell = {SpellID = 48707},
+            BloodBoil = {SpellID = 50842},
+            Blooddrinker = {SpellID = 206931},
+            Bonestorm = {SpellID = 194844},
+            Consumption = {SpellID = 274156},
+            ControlUndead  = {SpellID = 111673},
+            DancingRuneWeapon = {SpellID = 49028},
+            DarkCommand = {SpellID = 56222},
+            DeathAndDecay = {SpellID = 43265, SpellType = "Ground"},
+            DeathGrip = {SpellID = 49576},
+            DeathStrike = {SpellID = 49998},
+            DeathsAdvance = {SpellID = 48265},
+            DeathsCaress = {SpellID = 195292},
+            GorefiendsGrasp = {SpellID = 108199},
+            HeartStrike = {SpellID = 206930},
+            MarkOfBlood = {SpellID = 206940},
+            Marrowrend = {SpellID = 195182},
+            MindFreeze = {SpellID = 47528, SpellType = "Interrupt"},
+            PathOfFrost = {SpellID = 3714},
+            RaiseAlly = {SpellID = 61999},
+            RuneStrike = {SpellID = 210764},
+            RuneTap = {SpellID = 194679},
+            Tombstone = {SpellID = 219809},
+            VampiricBlood = {SpellID = 55233},
+            WraithWalk = {SpellID = 212552}
+        },
+        Buffs = {
+            AntiMagicBarrier = 205727,
+            AntiMagicShell = 48707,
+            BoneShield = 195181,
+            DancingRuneWeapon = 81256,
+            PathOfFrost = 3714
+        },
+        Debuffs = {
+            BloodPlague = {SpellID = 55078}
+        },
+        Talents = {
+            Heartbreaker = 19165,
+            Blooddrinker = 19166,
+            RuneStrike = 19217,
+            RapidDecomposition = 19218,
+            Hemostasis = 19219,
+            Consumption = 19220,
+            FoulBulwark = 19221,
+            Ossuary = 22134,
+            Tombstone = 22135,
+            WillOfTheNecropolis = 22013,
+            AntiMagicBarrier = 22014,
+            RuneTap = 22015,
+            GripOfTheDead = 19227,
+            TighteningGrasp = 19226,
+            WraithWalk = 19228,
+            Voracious = 19230,
+            Bloodworms = 19231,
+            MarkOfBlood = 19232,
+            Purgatory = 21207,
+            RedThirst = 21208,
+            Bonestorm = 21209
+        },
+        Traits = {
+            BloodyRuneblade = 289339,
+            BoneSpikeGraveyard = 273088,
+            BonesOfTheDamned = 278484,
+            ColdHearted = 288424,
+            DeepCuts = 272684,
+            EternalRuneWeapon = 278479,
+            MarchOfTheDamned = 280011,
+            Marrowblood = 274057,
+            RunicBarrier = 280010,
+        }
     },
     Frost = {
         Abilities = {},
@@ -23,7 +90,11 @@ Spells.DEATHKNIGHT = {
         Traits = {}
     },
     All = {
-        Abilities = {},
+        Abilities = {
+            AntiMagicShell = {SpellID = 48707},
+            Asphyxiate = {SpellID = 221562},
+            IceboundFortitude = {SpellID = 48792}
+        },
         Buffs = {},
         Debuffs = {}
     }
