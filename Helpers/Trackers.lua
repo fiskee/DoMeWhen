f9713d6aa163f21a2c9ec3c007317e1f1db5cff1
Fiskee <fisker@gmail.com>
2020-08-03 00:13:06 +0200

    Added mount rares

--- Helpers/Trackers.lua
+++ Helpers/Trackers.lua
@@ -5,6 +5,34 @@ local LibDraw = LibStub("LibDraw-1.0")
 local tX, tY, tZ
 local Settings
 
+function DMW.Helpers.DrawLineDMW(sx, sy, sz, ex, ey, ez)
+    local function WorldToScreen(wX, wY, wZ)
+        local sX, sY = _G.WorldToScreen(wX, wY, wZ)
+        if sX and sY then
+            return sX, -(WorldFrame:GetTop() - sY)
+        else
+            return sX, sY
+        end
+    end
+    local startx, starty = WorldToScreen(sx, sy, sz)
+    local endx, endy = WorldToScreen(ex, ey, ez)
+    if (endx == nil or endy == nil) and (startx and starty) then
+        local i = 1
+        while (endx == nil or endy == nil) and i < 50 do
+            endx, endy = WorldToScreen(GetPositionBetweenPositions(ex, ey, ez, sx, sy, sz, i))
+            i = i + 1
+        end
+    end
+    if (startx == nil or starty == nil) and (endx and endy) then
+        local i = 1
+        while (startx == nil or starty == nil) and i < 50 do
+            startx, starty = WorldToScreen(GetPositionBetweenPositions(sx, sy, sz, ex, ey, ez, i))
+            i = i + 1
+        end
+    end
+    LibDraw.Draw2DLine(startx, starty, endx, endy)
+end
+
 function DMW.Helpers.Trackers.Run()
     if not Settings then
         Settings = DMW.Settings.profile
@@ -19,8 +47,17 @@ function DMW.Helpers.Trackers.Run()
             LibDraw.Line(tX - s, tY, tZ, tX + s, tY, tZ)
             LibDraw.Line(tX, tY - s, tZ, tX, tY + s, tZ)
         end
-        if Settings.Developer.Units then
+        if Settings.Developer.Units or (DMW.Enums.Tracker.MountsRare[Unit.ObjectID] and not Unit.Dead) then
             LibDraw.Text("Unit: " .. Unit.Name .. " (" .. Unit.ObjectID .. ") - " .. math.floor(Unit.Distance) .. " Yards", "GameFontNormal", Unit.PosX, Unit.PosY, Unit.PosZ + 2)
+        end
+        if DMW.Enums.Tracker.MountsRare[Unit.ObjectID] and not Unit.Dead then
+            if (AlertTimer + 5) < DMW.Time and not IsForeground() then
+                FlashClientIcon()
+                AlertTimer = DMW.Time
+            end
+            tX, tY, tZ = Unit.PosX, Unit.PosY, Unit.PosZ
+            LibDraw.SetWidth(4)
+            DMW.Helpers.DrawLineDMW(tX, tY, tZ, DMW.Player.PosX, DMW.Player.PosY, DMW.Player.PosZ + 2)
         end
     end
     for _, Object in pairs(DMW.GameObjects) do
